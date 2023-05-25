ea415a442c21be50214645c2baf399ffdf99974b
AnthonyBertottiFavero <bertottianthony1@gmail.com>
2023-05-25 16:37:38 +0200

    add commit

---
+++ script/script.js
@@ -0,0 +1,57 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            toDos: [
+                {
+                    text: 'Fare la spesa',
+                    done: false
+                },
+
+                {
+                    text: 'Fare esercizio Boolean',
+                    done: false
+                },
+
+                {
+                    text: 'Pulire la camera',
+                    done: false
+                },
+
+                {
+                    text: 'Andare in palestra',
+                    done: false
+                },
+            ],
+
+            newTodo: '',
+
+            inputError: false,
+        }
+    },
+    methods: {
+        addTodo(){
+            let cleanedTodo = this.newTodo.trim();
+            if(cleanedTodo.length >= 5) {
+                this.toDos.unshift({
+                    text: this.newTodo,
+                    done: false,
+                });
+                this.newTodo = '';
+                this.inputError = false
+            } else {
+                this.inputError = true;
+            }
+        },
+
+        deleteItem(index) {
+            this.toDos.splice(index, 1);
+            this.toDos[index].done = !this.toDos[index].done;
+        },
+
+        jobDone(i){
+            this.toDos[i].done = !this.toDos[i].done;
+        }
+    }
+});
+
+app.mount('#app');
